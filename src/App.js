a4dec6855b63a22942ac7c358912f90289b76a0e
antonio kim <antoniokim@antonios-Air.fios-router.home>
2020-05-11 14:58:37 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactFCCtest from 'react-fcctest';
+import marked from 'marked';
 
-function App() {
+import './App.scss';
+
+const initialState = `
+# heading
+## heading 2
+
+**BOLD TEXT**
+
+[My Website](https://www.yahoo.com)
+
+This is a inline \`<div></div>\`
+
+This is a block of code
+\`\`\`
+let x = 1;
+let y = 2;
+let z = 3
+\`\`\`
+
+![React](https://goo.gl/Umyytc)
+`
+
+
+class App extends React.Component {
+  state = {
+    text : initialState
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      text : e.target.value
+    })
+  }
+
+  render() {
+    const {text} = this.state;
+    const markdown = marked(text);
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ReactFCCtest />
+    <h1>Mardown Preview</h1>
+      <div className="App-inner">
+        <div className="row">
+          <div className="preview" id="preview">
+            <div className="preview-header">
+              <h2>Preview</h2>
+            </div>
+            <div className="preview-textarea">
+              <textarea name="" id="editor" value={text} onChange={this.handleChange}></textarea>
+            </div>
+          </div>
+          <div className="editor" id="editor">
+            <div className="editor-header">
+              <h2>Editor</h2>
+            </div>
+            <div className="editor-textarea">
+              <div className="editor-textarea-inner">
+                <div dangerouslySetInnerHTML={{__html: markdown}}></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
+}
 }
 
 export default App;
